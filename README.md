74c4c07b5a78ce5afbfd38d61345d62c64bfb700
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-05-25 19:26:38 +0800

    hn_flutter stage 1 - DONE

---
+++ README.md
@@ -0,0 +1,3 @@
+# HackerNews Flutter
+
+The dummy app that based on my favorite Youtube channel.
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:hn_flutter/src/articles.dart';
+
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'HackerNews',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'HackerNews'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Article> _articles = articles;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        child: ListView(children: _articles.map(_buildItems).toList()),
+        onRefresh: () async {
+          await Future.delayed(const Duration(seconds: 1));
+
+          setState(() {
+            _articles.removeAt(0);
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildItems(Article article) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ExpansionTile(
+        title: Text(
+          article.text,
+          style: TextStyle(fontSize: 24.0),
+        ),
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text("${article.commentsCount} comments"),
+              IconButton(
+                onPressed: () async {
+                  final fakeUrl = "http://${article.domain}";
+
+                  if (await canLaunch(fakeUrl)) {
+                    launch(fakeUrl);
+                  }
+                },
+                icon: Icon(Icons.launch),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
